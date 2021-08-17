953b785668d0f6b9c7cee12f2f23df384eebf6bf
Chris Co <chrco@microsoft.com>
2021-08-17 02:14:07 +0000

    container-postinstall: add more post install steps
    Signed-off-by: Chris Co <chrco@microsoft.com>

--- toolkit/imageconfigs/postinstallscripts/enable-container-runtime.sh
+++ toolkit/imageconfigs/postinstallscripts/enable-container-runtime.sh
@@ -3,4 +3,27 @@
 # Enable docker service to start by default
 systemctl enable docker
 
+# Workaround issue where containerd service fails due to the service expecting
+# modprobe of overlay module to succeed before starting the service. In some of
+# our configurations, the overlay module is built into kernel instead of as an
+# loadable kernel module
 sed -i '/ExecStartPre/d' /lib/systemd/system/containerd.service
+
+# For now, disable all iptables rules (allow all)
+cat <<EOF >> /etc/systemd/scripts/iptables
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+#Then flush the rules:
+iptables -F INPUT
+iptables -F OUTPUT
+iptables -F FORWARD
+ 
+ip6tables -P INPUT ACCEPT
+ip6tables -P OUTPUT ACCEPT
+ip6tables -P FORWARD ACCEPT
+#Then flush the rules:
+ip6tables -F INPUT
+ip6tables -F OUTPUT
+ip6tables -F FORWARD
+EOF
